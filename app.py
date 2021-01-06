e3aec772615c9db94bbff49e2180292cef130faa
abcrp00 <abcrp00@gmail.com>
2021-01-07 00:00:00 +0530

    Commit on 2021-01-07 00:00:00 days ago

--- app.py
+++ app.py
@@ -2,25 +2,29 @@ import os
 from random import randint
 from datetime import datetime, timedelta
 
-# Set the start and end dates for the commits
-start_date = datetime(2021, 3, 1)
-end_date = datetime(2021, 1, 31)
+start_date = datetime(2021, 1, 1)  # Set your start date
+end_date = datetime(2021, 3, 28)   # Set your end date
 
-# Generate commits between January and March 2023
-for i in range(1, 10):
-    for j in range(0, randint(1, 10)):
-        # Calculate the date of the commit based on the range of days
-        commit_date = start_date + timedelta(days=i)
+commits_per_day = 1  # Number of commits per day
+gap_range = (1, 7)   # Range for the gap between commits (in days)
+
+current_date = start_date
+
+while current_date <= end_date:
+    for _ in range(commits_per_day):
+        gap = randint(*gap_range)
+        commit_date = current_date + timedelta(days=gap)
+
         if commit_date > end_date:
             break
-        
-        # Create a commit message and format the date
+
         d = commit_date.strftime("%Y-%m-%d %H:%M:%S") + " days ago"
         
         with open('file.txt', 'a') as file:
             file.write(d + "\n")
         
-        # Add the changes, commit with a unique message, and push
         os.system('git add .')
         os.system(f'git commit --date="{commit_date.strftime("%a %b %d %H:%M:%S %Y")}" -m "Commit on {d}"')
         os.system('git push -u origin main')
+
+    current_date += timedelta(days=1)
